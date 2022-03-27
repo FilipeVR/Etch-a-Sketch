8f5419b9c1d32f07f92a6266c3d682ec7a91e58e
Filipe <filipeviegasribeiro@gmail.com>
2022-03-27 20:15:50 -0300

    Add settings menu
    Add divs to index.html with settings buttons. Turn the grid into a function to implement user input reaction properly in EAS.js and make clear button clean the grid on click by turning the grid's divs background color back to it's original color. Style the position of the grid and the menu in the page as the page's colors in EAS.css.

--- EAS.js
+++ EAS.js
@@ -1,41 +1,45 @@
 const ct = document.querySelector('.container');
 
-for(let i = 0; i<16; i++) {
-    let row = document.createElement('div');
-    row.id = `row_${i+1}`;
-    row.className = "row";
-    for(let j = 1; j<=16; j++) {
-        let pxsqr = document.createElement('div');
-        pxsqr.id = `gridPixel_${(i*16)+j}`;
-        pxsqr.className = "gridPixel";
-        row.appendChild(pxsqr);
+function grid(size) {
+    for(let i = 0; i<size; i++) {
+        let row = document.createElement('div');
+        row.id = `row_${i+1}`;
+        row.className = "row";
+        for(let j = 1; j<=size; j++) {
+            let pxsqr = document.createElement('div');
+            pxsqr.id = `gridPixel_${(i*size)+j}`;
+            pxsqr.className = "gridPixel";
+            row.appendChild(pxsqr);
+        }
+        ct.appendChild(row);
     }
-    ct.appendChild(row);
 }
+
+grid(100);
 
 const gdpx = document.querySelectorAll('.gridPixel')
 
 let mouseDown = false;//used in the 'click+drag' paint feature
 
-Array.from(gdpx).forEach((gd) => {
+Array.from(gdpx).forEach((px) => {
 
     //******** Paint the grid only on 'click+drag' event ********/
 
     /*Listen to 'mousedown' event and update mouseDown variable*/
-    gd.addEventListener('mousedown', function(ev) {
+    px.addEventListener('mousedown', function(ev) {
         ev.preventDefault();
         mouseDown = true;
     },false);
 
     /*Listen to 'mousemove' event and apply dragPaint()*/
-    gd.addEventListener('mousemove', function(ev) {
+    px.addEventListener('mousemove', function(ev) {
         if (!mouseDown) {return} //check if the button is currently pressed to proceed with the execution.
         ev.preventDefault();
         dragPaint(ev);
     }, false);
 
     /*Listen to 'mouseup' event and update mouseDown variable*/
-    gd.addEventListener('mouseup', function(ev) {
+    px.addEventListener('mouseup', function(ev) {
         ev.preventDefault();
         mouseDown = false;
     }, false)
@@ -45,4 +49,16 @@ Array.from(gdpx).forEach((gd) => {
     }
     
     //******** Paint the grid only on 'click+drag' event ********/
+})
+
+const menu = document.querySelector('.menu');
+
+const clearGrid = document.createElement('button');
+clearGrid.textContent = "Clear";
+menu.appendChild(clearGrid);
+
+clearGrid.addEventListener('click', () => {
+    Array.from(gdpx).forEach((px) => {
+        px.setAttribute('style','background: white;');
+    })
 })
